9a7f0de111ed2cf770b716e3bad7d950331fbb2a
sam ghoreishi <sam20gh@gmail.com>
2019-01-01 17:01:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -76,16 +76,41 @@ function takeANumber(katzDeli, name){
   }
   return (`Welcome, ${name}. You are number ${katzDeli.length} in line.`)
 }
+
+
 function nowServing(katzDeli){
   if (katzDeli.length === 0){
     return `There is nobody waiting to be served!`
     } else{
-      var katzDeliline = katzDeli.push();
-     return `Now serving ${katzDeliline}`;
+      var servingName = katzDeli.push();
+     return `Now serving ${servingName}`;
     }
     
 }
 
+
+function currentLine(katzDeli, name){
+  if (katzDeli.length === 0){
+    return `The line is currently empty.`;
+  }else{
+  var newLine = [];
+  for (var i = 0; i < katzDeli.length; i++){
+    newLine.push((i + 1) + ". " + katzDeli[i] + ",")
+  } return `The line is currently:${newLine}`
+}
+}
+
+function currentLine(katzDeli, name) {
+  let i = 0;
+  while (i < katzDeli.length) {
+    line.push(` `+[i+1]+`. `  + katzDeli[i])
+    i++;
+  }
+  if (katzDeli.length === 0) {
+    return "The line is currently empty.";
+  } else
+  return(`The line is currently:` + line);
+}
 
 
 
